870fefd118149e639774426c1015d06bb86b6a78
AndrasHorinka <horinka.andras@gmail.com>
2019-04-05 12:06:12 +0200

    init

---
+++ SimpleThreadingDemo/SimpleThreadingDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleThreadingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+        public static void Counting()
+        {
+            int countLimit = 10;
+            for (int i = 0; i < countLimit; i++)
+            {
+                Console.WriteLine("Thread: {}, Count {}", Thread.CurrentThread.ManagedThreadId, i);
+            }
+        }
+    }
+}
