398208e86e771327f8e57f3d512094b7ba0289ea
AndrasHorinka <horinka.andras@gmail.com>
2019-04-06 09:41:12 +0200

    ThreadPoolDemo

---
+++ SimpleThreadingDemo/ThreadPoolDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadPoolDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WaitCallback thread = new WaitCallback(ShowMyText);
+
+            try
+            {
+                ThreadPool.QueueUserWorkItem(thread, "elso text");
+                ThreadPool.QueueUserWorkItem(thread, "masodik text");
+                ThreadPool.QueueUserWorkItem(thread, "harmadik text");
+                ThreadPool.QueueUserWorkItem(thread, "negyedik text");
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        static void ShowMyText(object state)
+        {
+            string newVariable = (string)state;
+            Console.WriteLine(newVariable + " " + Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
